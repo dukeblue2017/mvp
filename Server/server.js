1cf1bf03659e8d52ff1acf2d7dfb13f5d28a959e
Hack Reactor Student <student@hackreactor.com>
2017-11-01 12:24:57 -0500

    Created 'Server' directory and 'server.js' - created server

---
+++ Server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000, function() {
+  console.log('Bonfire listening on port 3000');
+})
+
+app.get('/', function(req, res) {
+  console.log('GET received to /');
+  res.send('Hello World!');
+})
