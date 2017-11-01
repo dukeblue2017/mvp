0546e6438e2e1fd8f8a58f05583755cbbaaa03a2
Hack Reactor Student <student@hackreactor.com>
2017-11-01 15:24:16 -0500

    Added a script based off of dalyhabit's example to create a database, not yet functioning

---
+++ database-mysql/schema.sql
@@ -0,0 +1,11 @@
+-- this file is not yet in use
+
+CREATE DATABASE bonfire_db;
+
+USE bonfire_db;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  NAME varchar(200),
+  PRIMARY KEY (ID)
+)
