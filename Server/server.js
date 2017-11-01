022bb8b70a327c075be2331871cd07576fa52ab2
Hack Reactor Student <student@hackreactor.com>
2017-11-01 14:34:38 -0500

    Required database folder in server.js, changed name of the exported query function to reflect function naming conventions

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+// var queryDB = require('dbQuery');
+var dbIndex = require('../database-mysql/')
+
+console.log('SELECT test', dbIndex.queryDB('SELECT * FROM USERS'));
 
 app.listen(3000, function() {
   console.log('Bonfire listening on port 3000');
@@ -7,6 +11,7 @@ app.listen(3000, function() {
 
 app.get('/', function(req, res) {
   console.log('GET received to /');
+  // console.log( queryDB('SELECT * FROM users') );
   res.send('Welcome to Bonfire!');
 })
 
