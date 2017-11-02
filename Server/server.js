0c71948ffb14997c04e8e4cbc8999f72c3e4d4f8
Hack Reactor Student <student@hackreactor.com>
2017-11-02 12:41:10 -0500

    updated server and database, see comments in server.js for guidance on how to move forward

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-// var queryDB = require('dbQuery');
 var dbIndex = require('../database-mysql/')
 
 app.listen(3000, function() {
@@ -9,20 +8,61 @@ app.listen(3000, function() {
 
 app.get('/', function(req, res) {
   console.log('GET received to /');
-  // console.log( queryDB('SELECT * FROM users') );
+  //need to send index.html, not this
   res.send('Welcome to Bonfire!');
+})
+
+app.post('/', function(req, res) {
+  console.log('POST received to /');
+  res.sendStatus(405);
 })
 
 app.get('/users', function(req, res) {
   console.log('GET received to /users');
 
-  dbIndex.queryDB('SELECT * FROM USERS', function(err, data) {
+  dbIndex.queryDB('SELECT name FROM USERS', function(err, result) {
     if (err) {
-      res.sendStatus(500);
       console.log(err);
+      res.sendStatus(500);
     } else {
-      res.json(data);
-      console.log('data', data)
+      console.log('result', result);
+      res.json(result);
     }
   });
+})
+
+app.post('/users', function(req, res) {
+  console.log('POST received to /users');
+  // need to change 'Hans'
+  var sqlText = 'INSERT INTO users (name) VALUES (\'Hans\')';
+  dbIndex.queryDB(sqlText, function(err, result) {
+    if (err) {
+      console.log('err');
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(201);
+    }
+  })
+})
+
+app.get('/profile', function(req, res) {
+  console.log('GET received to /profile');
+  // need to insert username here at the end of query:
+  var sqlText = 'SELECT name, song_title, song_artist, song_album FROM users;'
+  dbIndex.queryDB(sqlText, function(err, result) {
+    if (err) {
+      console.log('err');
+      res.sendStatus(500);
+    } else {
+      console.log('result', result);
+      res.json(result);
+    }
+  })
+})
+
+app.post('/profile', function(req, res) {
+  console.log('POST received to /profile');
+  // this needs to update the user's favorite song;
+  res.send()
+
 })
--- database-mysql/dropAndRedobonfire_db.sql
+++ database-mysql/dropAndRedobonfire_db.sql
@@ -6,12 +6,15 @@ USE bonfire_db;
 
 CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
-  NAME varchar(200),
+  name varchar(200),
+  song_title varchar(200),
+  song_artist varchar(200),
+  song_album varchar(200),
   PRIMARY KEY (ID)
 );
 
-INSERT INTO users (name)
-VALUES ('Jonny');
+INSERT INTO users (name, song_title, song_artist, song_album)
+VALUES ('Jonny', 'Danza Kuduro', 'Don Omar ft. Lucenzo', 'Don Omar Presents: Meet the Orphans');
 
-INSERT INTO users(name)
-VALUES ('Carter');
+INSERT INTO users(name, song_title, song_artist, song_album)
+VALUES ('Carter', 'Lenny', 'Stevie Ray Vaughan & Double Trouble', 'Texas Flood');
