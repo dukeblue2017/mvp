bd5c3e3569901b21bfcd5d5492abe2b248bf7bd0
Hack Reactor Student <student@hackreactor.com>
2017-11-02 12:48:05 -0500

    Made progress on the angular client. Still not really functional. Breaking for lunch.

--- client-angular/components/postSong.js
+++ client-angular/components/postSong.js
@@ -1,4 +1,12 @@
-angular.module('bonfire', [])
-  .controller('PostController', function() {
-
+angular.module('bonfire')
+  .controller('postController', function() {
+    console.log('hi')
+    this.makeAPost = function() {
+      console.log('making a post')
+    }
+  })
+  .component('songForm', {
+    bindings: {},
+    controller: 'postController',
+    template: `<div>Hello</div>`
   })
