e41c2206e2338234d1a174ea55dfdc57b4c2ba4d
Carter Duncan <dukeblue2017@gmail.com>
2017-11-02 19:56:19 -0500

    Added forms for song title, artist, and album

--- client-angular/components/songForm.js
+++ client-angular/components/songForm.js
@@ -1,15 +1,15 @@
 angular.module('bonfire')
   .controller('songFormCtrl', function($http) {
     console.log('{{yourName}}')
-    this.makeAPost = function(who) {
+    this.makeAPost = function(name, title, artist, album) {
       $http({
         method: 'POST',
         url: '/users',
         data: {
-          username: who,
-          songTitle: 'abc',
-          songArtist: '123',
-          songAlbum: 'abc123'
+          username: name,
+          songTitle: title,
+          songArtist: artist,
+          songAlbum: album
         }
       }).then(function successCallback(response) {
           console.log('success posting to users', response)
@@ -25,8 +25,12 @@ angular.module('bonfire')
       <div class="songform">
         <label>Username:</label>
         <input type="text" ng-model="yourName" placeholder="Enter username here">
+        <input type="text" ng-model="songTitle" placeholder="Enter song title here">
+        <input type="text" ng-model="songArtist" placeholder="Enter artist name here">
+        <input type="text" ng-model="songAlbum" placeholder="Enter album title here">
+
         <h1>{{yourName}}</h1>
-        <button ng-click="$ctrl.makeAPost(yourName)">Submit New User</button>
+        <button ng-click="$ctrl.makeAPost(yourName, songTitle, songArtist, songAlbum)">Submit New User</button>
       </div>
     `
   })
